d463470a28fbb5aadd60aefa28c21cd9db5bb93d
chao <chao@chaos-MacBook-Air.local>
2018-08-06 17:56:47 +0800

    add index.php

---
+++ index.php
@@ -0,0 +1,19 @@
+<html>
+ <head>
+  <title>PHP Test</title>
+ </head>
+ <body>
+
+	<?php 
+		for ($j = 0; $j<3; $j++) 
+	?>
+	this is a test
+	<?php { echo $j; ?>
+	<br>
+	<?php } ?>
+
+<br>
+
+
+ </body>
+</html>
